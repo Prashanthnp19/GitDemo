b42a5536f1fc7934080d55db115f01946e7f3d73
prashanth <prashanthnp19@gmail.com>
2021-08-05 21:31:45 +0530

    ff

--- src/test/java/dummy/RESTAPITest.java
+++ src/test/java/dummy/RESTAPITest.java
@@ -6,22 +6,12 @@ public class RESTAPITest {
 	@Test
 	public void login() {
 		System.out.println("Login API");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
 		
 	}
 	
 	@Test 
 	public void logout() {
 		System.out.println("Logout API");
-		System.out.println("Logout API from Web application");
+		
 	}
 }
