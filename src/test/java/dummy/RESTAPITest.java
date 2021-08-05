d09931bd9628148dc1ab5e541d5586209313848d
prashanth <prashanthnp19@gmail.com>
2021-08-05 19:17:15 +0530

    Indian architect code

--- src/test/java/dummy/RESTAPITest.java
+++ src/test/java/dummy/RESTAPITest.java
@@ -6,26 +6,11 @@ public class RESTAPITest {
 	@Test
 	public void login() {
 		System.out.println("Login API");
-		System.out.println("Login API from Web application");
-
-		
 	}
-	
-	@Test 
+
+	@Test
 	public void logout() {
 		System.out.println("Logout API");
-		
-	}
-	
-	@Test 
-	public void password() {
-		System.out.println("password API");
-		
-	}
-	
-	@Test 
-	public void username() {
-		System.out.println("username API");
-		
+
 	}
 }
--- src/test/java/dummy/SeleniumTest.java
+++ src/test/java/dummy/SeleniumTest.java
@@ -6,21 +6,5 @@ public class SeleniumTest {
 	@Test
 	public void Browsertest() {
 		System.out.println("Browser test");
-		System.out.println("Chrome Browser");
-	}
-	
-	@Test 
-	public void elementsUI() {
-		System.out.println("ElementsUI test");
-	}
-	
-	@Test 
-	public void Microsoft() {
-		System.out.println("Microsoft browser test");
-	}
-	
-	@Test 
-	public void internetExlorer() {
-		System.out.println("InternetExplorer test");
 	}
 }
