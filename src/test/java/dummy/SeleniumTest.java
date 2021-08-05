44c1731670014719963622af359ade63532023c2
prashanth <prashanthnp19@gmail.com>
2021-08-05 20:37:44 +0530

    Indian testing team

--- src/test/java/dummy/SeleniumTest.java
+++ src/test/java/dummy/SeleniumTest.java
@@ -8,19 +8,4 @@ public class SeleniumTest {
 		System.out.println("Browser test");
 		System.out.println("Chrome Browser");
 	}
-	
-	@Test 
-	public void elementsUI() {
-		System.out.println("ElementsUI test");
-	}
-	
-	@Test 
-	public void Microsoft() {
-		System.out.println("Microsoft browser test");
-	}
-	
-	@Test 
-	public void internetExlorer() {
-		System.out.println("InternetExplorer test");
-	}
 }
