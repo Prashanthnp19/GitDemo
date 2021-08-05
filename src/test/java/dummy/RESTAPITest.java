6ddae310d35b6b63a78a509bf464495684d632a7
prashanth <prashanthnp19@gmail.com>
2021-08-05 19:04:16 +0530

    Architect Asian Development

--- src/test/java/dummy/RESTAPITest.java
+++ src/test/java/dummy/RESTAPITest.java
@@ -7,21 +7,25 @@ public class RESTAPITest {
 	public void login() {
 		System.out.println("Login API");
 		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
-		System.out.println("Login API from Web application");
+
 		
 	}
 	
 	@Test 
 	public void logout() {
 		System.out.println("Logout API");
-		System.out.println("Logout API from Web application");
+		
+	}
+	
+	@Test 
+	public void password() {
+		System.out.println("password API");
+		
+	}
+	
+	@Test 
+	public void username() {
+		System.out.println("username API");
+		
 	}
 }
--- src/test/java/dummy/SeleniumTest.java
+++ src/test/java/dummy/SeleniumTest.java
@@ -7,18 +7,20 @@ public class SeleniumTest {
 	public void Browsertest() {
 		System.out.println("Browser test");
 		System.out.println("Chrome Browser");
-		System.out.println("Chrome Browser");
-		System.out.println("Chrome Browser");
-		System.out.println("Chrome Browser");
-		System.out.println("Chrome Browser");
-		System.out.println("Chrome Browser");
-		System.out.println("Chrome Browser");
-		System.out.println("Chrome Browser");
-		System.out.println("Chrome Browser");
 	}
 	
 	@Test 
 	public void elementsUI() {
 		System.out.println("ElementsUI test");
+	}
+	
+	@Test 
+	public void Microsoft() {
+		System.out.println("Microsoft browser test");
+	}
+	
+	@Test 
+	public void internetExlorer() {
+		System.out.println("InternetExplorer test");
 	}
 }
