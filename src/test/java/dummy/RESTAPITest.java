95816cb27cbd7bbfbbe0bc167e2dd78beb836d6a
prashanth <prashanthnp19@gmail.com>
2021-08-06 11:57:04 +0530

    tt

--- src/test/java/dummy/RESTAPITest.java
+++ src/test/java/dummy/RESTAPITest.java
@@ -5,13 +5,11 @@ import org.testng.annotations.Test;
 public class RESTAPITest {
 	@Test
 	public void login() {
-		System.out.println("Login API");
-		
+		System.out.println("Login API");	
 	}
 	
 	@Test 
 	public void logout() {
-		System.out.println("Logout API");
-		
+		System.out.println("Logout API");	
 	}
 }
